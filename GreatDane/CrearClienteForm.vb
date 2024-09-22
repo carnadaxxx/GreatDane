3bcf05e692489bb723f7ff78ac4f1c9cbd218bf9
wilson coronado barrantes <w.coronadobarrantes@gmail.com>
2024-09-22 17:57:03 -0500

    first commit

---
+++ GreatDane/CrearClienteForm.vb
@@ -0,0 +1,5 @@
+﻿Public Class CrearClienteForm
+    Private Sub BtnCrearCliente_Click(sender As Object, e As EventArgs) Handles BtnCrearCliente.Click
+        MessageBox.Show("Hello world")
+    End Sub
+End Class
